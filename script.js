5ed550b94128579aa6fd9c5af0a400e64b9dbe0f
gabi <gaby.gabyyy29@gmail.com>
2022-09-14 01:48:37 +0300

    Add the functionality of adding books to the dom
    Now when opening the modal and submitting the form in it, a new book element will be created and rendered on the page.

--- script.js
+++ script.js
@@ -15,8 +15,20 @@ const logoNameEl = document.querySelector(".sidebar__title");
 const navTooltipEl = document.querySelectorAll(".nav__tooltip")
 const navLinkTextEls = document.querySelectorAll(".nav__link-text");
 const mainContainerEl = document.querySelector(".main-container");
+const horizontalRuleEl = document.querySelector(".horizontal-rule");
+const bookItemEl = document.querySelector(".book-item");
+const addBookLinkEl = document.querySelector(".nav__add-book-link");
+const overlayEl = document.querySelector(".overlay");
+const modalEl = document.querySelector(".modal");
+const modalCloseBtn = document.querySelector(".modal__close-button");
+const addBookBtn = document.querySelector('.new-book__add-book-btn');
+const formTitleInput = document.querySelector('.new-book__title-input');
+const formAuthorInput = document.querySelector('.new-book__author-input');
+const formEl = document.querySelector(".add-new-book-form");
+const bookshelfContainer = document.querySelector(".bookshelf-container");
 
-menuBtn.addEventListener("click", ()=>{
+
+menuBtn.addEventListener("click", () => {
 
   toggleSidebar();
   toggleTooltips();
@@ -24,10 +36,30 @@ menuBtn.addEventListener("click", ()=>{
   toggleMenuBtn();
   toggleNavLinkTextEls();
   toggleMainContainerWidth();
+  toggleHorizontalRule();
+
+  bookItemEl.classList.remove("hidden");
+  bookItemEl.classList.toggle("invisible");
+  if(bookItemEl.classList.contains("invisible")){
+    bookItemEl.classList.add("hidden");
+    setTimeout( () => {
+      bookItemEl.classList.remove("hidden");
+    }, 1);
+  }
 });
 
+modalCloseBtn.addEventListener("click", () => {
+  modalEl.classList.add("hidden");
+  overlayEl.classList.add("hidden");
+});
+
+formEl.addEventListener("submit", addBookOnPage);
+
+
+
+
 function toggleMenuBtn() {
- if(sidebar.classList.contains("open")){
+ if(sidebar.classList.contains("sidebar--open")){
    menuBtn.classList.replace("fa-bars", "fa-xmark");//replacing the iocns class
  }else {
    menuBtn.classList.replace("fa-xmark","fa-bars");//replacing the iocns class
@@ -71,4 +103,43 @@ function toggleNavLinkTextEls(){
 
 function toggleMainContainerWidth(){
   mainContainerEl.classList.toggle("main-container-reduced");
+}
+
+function toggleHorizontalRule(){
+  horizontalRuleEl.classList.toggle("invisible");
+}
+
+addBookLinkEl.addEventListener("click", function(){
+  modalEl.classList.remove("hidden");
+  overlayEl.classList.remove("hidden");
+});
+
+overlayEl.addEventListener("click", () => {
+  if(!modalEl.classList.contains("hidden")){
+    modalEl.classList.add("hidden");
+    overlayEl.classList.add("hidden");
+  }
+})
+
+function addBookOnPage(e){
+  e.preventDefault();
+  console.log(bookshelfContainer);
+  const title = formTitleInput.value;
+  const author = formAuthorInput.value;
+  let htmlEl = document.createElement('div');
+  overlayEl.classList.add("hidden");
+  modalEl.classList.add("hidden");
+  
+  htmlEl.classList.add('book');
+  
+  htmlEl.innerHTML = `
+  <div class="book__data">
+  <h3 class="book__title">${title}</h3>
+  <h4 class="book__author"><span class="book__by">${author.length > 0 ? "by" : ""}</span> <br> ${author}</h4>
+      </div>
+      `;
+  // let bookNode = document.createTextNode(htmlEl);
+      
+  bookshelfContainer.appendChild(htmlEl);
+
 }
