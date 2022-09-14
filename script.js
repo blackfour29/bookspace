99e08a7d0803c0aa35aa24c140019b498abc8ce4
gabi <gaby.gabyyy29@gmail.com>
2022-09-14 04:44:16 +0300

    Implement add book illustration and card on page

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let myLibrary = [];
 
-function Book(){
   
+
+class Book{
+  constructor(bookElement, bookCardElement, bookData){
+    this.bookElement = bookElement;
+    this.bookCardElement = bookCardElement;
+    this.bookData = bookData;
+  }
 }
 
-function addBookToLibrary(){
-  
-}
 
 const sidebar = document.querySelector(".sidebar");
 const menuBtn = document.querySelector(".menu__icon");
@@ -16,7 +19,7 @@ const navTooltipEl = document.querySelectorAll(".nav__tooltip")
 const navLinkTextEls = document.querySelectorAll(".nav__link-text");
 const mainContainerEl = document.querySelector(".main-container");
 const horizontalRuleEl = document.querySelector(".horizontal-rule");
-const bookItemEl = document.querySelector(".book-item");
+const bookItemEl = document.querySelector(".book-card");
 const addBookLinkEl = document.querySelector(".nav__add-book-link");
 const overlayEl = document.querySelector(".overlay");
 const modalEl = document.querySelector(".modal");
@@ -24,8 +27,10 @@ const modalCloseBtn = document.querySelector(".modal__close-button");
 const addBookBtn = document.querySelector('.new-book__add-book-btn');
 const formTitleInput = document.querySelector('.new-book__title-input');
 const formAuthorInput = document.querySelector('.new-book__author-input');
+const formCommentsInput = document.querySelector(".new-book__comments")
 const formEl = document.querySelector(".add-new-book-form");
 const bookshelfContainer = document.querySelector(".bookshelf-container");
+const bookCardsContainer = document.querySelector(".book-card-list");
 
 
 menuBtn.addEventListener("click", () => {
@@ -123,23 +128,65 @@ overlayEl.addEventListener("click", () => {
 
 function addBookOnPage(e){
   e.preventDefault();
-  console.log(bookshelfContainer);
+
+
   const title = formTitleInput.value;
   const author = formAuthorInput.value;
-  let htmlEl = document.createElement('div');
+  const comments = formCommentsInput.value;
+
+  let bookIllustrationEl = document.createElement('div');
   overlayEl.classList.add("hidden");
   modalEl.classList.add("hidden");
   
-  htmlEl.classList.add('book');
+  bookIllustrationEl.classList.add('book');
   
-  htmlEl.innerHTML = `
+  bookIllustrationEl.innerHTML = `
   <div class="book__data">
   <h3 class="book__title">${title}</h3>
   <h4 class="book__author"><span class="book__by">${author.length > 0 ? "by" : ""}</span> <br> ${author}</h4>
       </div>
       `;
-  // let bookNode = document.createTextNode(htmlEl);
-      
-  bookshelfContainer.appendChild(htmlEl);
+  
+  let bookCardEl = document.createElement('div');
+  bookCardEl.classList.add('book-card');
 
+  bookCardEl.innerHTML = `
+    <h2 class="book-card__title">${title}</h2>
+    <p class="book-card__author"> <span>${author.length > 0 ? "by" : ""}</span> ${author}</p>
+    <p class="book-card__comments">${comments}</p>
+    <div class="book-card__buttons">
+      <i class="fa-solid fa-pen book-card__icon book-card__edit-icon"></i>
+      <i class="fa-solid fa-xmark book-card__icon "></i>
+    </div>
+  `
+  
+
+
+  let bookId = generateUniqueId();
+
+  bookIllustrationEl.setAttribute('id', bookId);
+  bookCardEl.setAttribute('id', bookId);
+
+  bookCardsContainer.appendChild(bookCardEl);
+  bookshelfContainer.appendChild(bookIllustrationEl);
+
+  bookCardEl.addEventListener("click", buttonPressed);
 }
+
+function buttonPressed(event){
+  if(event.target.classList.contains("book-card__icon")){
+    const elToDelete = event.target.closest('.book-card');
+
+    elToDelete.remove();
+  }
+}
+
+
+function generateUniqueId(){
+  let timestamp = Date.now();
+  let randomNumber = Math.floor(Math.random() * 10000);
+
+  let id = `${timestamp}${randomNumber}`;
+
+  return id;
+  }
